75c2b87e89841111f3a040fe2a0e33a9b94bd6d1
Mintesnot Bekele <mintesnotbekele23@gmail.com>
2023-12-15 18:54:37 +0300

    rols fix

--- resources/views/admins/create.blade.php
+++ resources/views/admins/create.blade.php
@@ -17,23 +17,7 @@
 				<li class="breadcrumb-item active">create admins</li>
 			</ol>
 		</div>
-		
-		<div class="card-header">
-                        <ul class="nav nav-tabs align-items-end card-header-tabs w-100">
-		<li class="nav-item">
-                                <a class="nav-link" href="{!! route('admins.assignRole') !!}"><i
-                                            class="fa fa-plus mr-2"></i>Assign Role</a>
-                            </li>
-		</ul>
-		</div>
-		<div class="card-header">
-                        <ul class="nav nav-tabs align-items-end card-header-tabs w-100">
-		<li class="nav-item">
-                                <a class="nav-link" href="{!! route('admins.createRole') !!}"><i
-                                            class="fa fa-plus mr-2"></i>Create Role</a>
-                            </li>
-		</ul>
-		</div>
+	
 
 		<div>
 		<form action="{{ route('admin.store') }}" method="post">
