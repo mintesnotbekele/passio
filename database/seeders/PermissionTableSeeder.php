8514a47e5b50b432dedf83550142888b23a4211d
Mintesnot Bekele <mintesnotbekele23@gmail.com>
2023-12-15 19:52:54 +0300

    permissions added

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -19,11 +19,19 @@ class PermissionTableSeeder extends Seeder
     public function run()
 
     {
+       
+
 
         $permissions = [
+            'VIEW-WALLET',' VIEW WALLET',' EDIT_WALLET_BALANCE',
+            'VIEW USERS', 'EDIT USERS' ,'DELETE_USERS','VIEW_RIDE',
+            'VIEW_PAYOUT', 'REQUEST_ONLY' , 'EDIT_PAYOUT' , 'APPROVE PAYOUT REQUEST',
+            'VIEW ORDERS' , 'EDIT ORDERS', 'CANCEL ORDERS',
+            'CREATE_PACKAGES', 'VIEW_PACKAGES', 'MODIFY_PACKAGES',
             'role-view', 'role-create', 'role-edit', 'role-delete',
             'permission-view', 'permission-create', 'permission-edit', 'permission-delete',
             'user-view', 'user-create', 'user-edit', 'user-delete'
+
         ];
         foreach ($permissions as $permission) {
             Permission::create(['name' => $permission, 'guard_name' => 'admin']);
